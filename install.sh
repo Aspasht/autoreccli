72fb029a6cd7e97c59b97318506720289801995d
Aspasht <72987297+Aspasht@users.noreply.github.com>
2024-02-12 15:23:28 +0300

    Create install.sh
    Added installation script

---
+++ install.sh
@@ -0,0 +1,7 @@
+go install github.com/lc/gau/v2/cmd/gau@latest
+go install github.com/tomnomnom/waybackurls@latest
+go install github.com/tomnomnom/assetfinder@latest
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+go install -v github.com/owasp-amass/amass/v4/...@master
+go install github.com/OJ/gobuster/v3@latest
+go install github.com/003random/getJS@latest
